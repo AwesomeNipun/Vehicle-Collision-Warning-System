0280c4e16343f8bb06c522eaa25da258018d1e54
Sanduni-Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2022-05-13 01:30:40 +0530

    danger zone
    Danger zone code

--- markPoints.py
+++ markPoints.py
@@ -1,59 +1,58 @@
 # importing the module
 import cv2
+import numpy as np
   
-# function to display the coordinates of
-# of the points clicked on the image
-def click_event(event, x, y, flags, params):
- 
+# function to detect the coordinates of danger zone
+# click on the 4 points and press space bar to exit
+def click_event(event, x, y, flags, pts):
     # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
- 
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)    
- 
-        # displaying the coordinates
-        # on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, str(x) + ',' +
-                    str(y), (x,y), font,
-                    1, (255, 0, 0), 2)
+        pts.append([x,y])
         cv2.imshow('image', img)
- 
-    # checking for right mouse clicks    
-    if event==cv2.EVENT_RBUTTONDOWN:
- 
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)
- 
-        # displaying the coordinates
-        # on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        b = img[y, x, 0]
-        g = img[y, x, 1]
-        r = img[y, x, 2]
-        cv2.putText(img, str(b) + ',' +
-                    str(g) + ',' + str(r),
-                    (x,y), font, 1,
-                    (255, 255, 0), 2)
-        cv2.imshow('image', img)
+    return pts
  
 # driver function
 if __name__=="__main__":
- 
+
+    # code to get the first frame of the input video
+    vidcap = cv2.VideoCapture('solidWhiteRight.mp4')
+    success,image = vidcap.read()
+    cv2.imwrite("frame1.jpg", image)
+
     # reading the image
-    img = cv2.imread('/home/sanduni/Documents/GitHub/Vehicle-Collision-Warning-System/image.JPG', 1)
+    img = cv2.imread('frame1.jpg', 1)
  
     # displaying the image
     cv2.imshow('image', img)
- 
+
+    # list to get the coordinates of danger zone
+    pts = []
+
     # setting mouse handler for the image
     # and calling the click_event() function
-    cv2.setMouseCallback('image', click_event)
- 
+    cv2.setMouseCallback('image', click_event,pts)
+    
     # wait for a key to be pressed to exit
     cv2.waitKey(0)
- 
+
+    print(pts)
     # close the window
+    cv2.destroyAllWindows()
+
+    # path
+    path = r'frame1.jpg'
+    
+    # Reading an image in default mode
+    image = cv2.imread(path)
+    
+    # Polygon corner points coordinates
+    pts = np.array(pts, np.int32)
+    print(pts)
+    
+    # Using cv2.polylines() method
+    image = cv2.polylines(image, [pts], True, (0, 0, 255), 2)
+
+    cv2.imshow('image', image)
+    
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
